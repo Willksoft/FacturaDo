1ef3f950e15aeed24ac7d966b5534900a55c9e80
Willksoft <willksoft@gmail.com>
2026-06-13 16:27:18 -0400

    Migrate from Electron to Capacitor

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.facturado.app',
+  appName: 'FacturaDo',
+  webDir: 'dist'
+};
+
+export default config;
--- electron/preload.js
+++
@@ -1,6 +0,0 @@
-const { contextBridge } = require('electron');
-
-contextBridge.exposeInMainWorld('electronAPI', {
-  // You can expose functions here if needed to communicate between frontend and Electron
-  isElectron: true
-});
